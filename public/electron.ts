81920e74226baf98db8ead3d07c8c34ad261a670
Han Sanggu <korean139@gmail.com>
2020-03-07 08:05:15 +0900

    feat(electron): add essential modules for electron
    this project will use electron to develop multi-platform app - so add
    modules for this

---
+++ public/electron.ts
@@ -0,0 +1,42 @@
+// public/electron.ts
+import { app, BrowserWindow } from 'electron';
+import * as isDev from 'electron-is-dev';
+import * as path from 'path';
+
+let mainWindow: BrowserWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    center: true,
+    kiosk: !isDev,
+    resizable: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../build/index.html'));
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = undefined!;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
