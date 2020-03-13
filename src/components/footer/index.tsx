c74085407093ab7530ba4cd7340ba330456e0b83
Han Sanggu <korean139@gmail.com>
2020-03-14 07:47:19 +0900

    feat(component): let footer indicate current page
    via selected property of button, let user know where they are

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 import Button from '../../components/button';
 import homeFeedIcon from '../../static/home-feed-icon.png';
@@ -13,28 +13,60 @@ interface FooterProps {
 export type Props = FooterProps & React.HTMLAttributes<HTMLDivElement>;
 
 const Footer: React.FC<Props> = (props) => {
+  const [currentPage, setCurrentPage] = useState('home-feed');
+
+  const onClickHomeFeed = () => {
+    setCurrentPage('home-feed');
+  }
+
+  const onClickExplore = () => {
+    setCurrentPage('explore');
+  }
+
+  const onClickCamera = () => {
+    setCurrentPage('camera');
+  }
+
+  const onClickLikes = () => {
+    setCurrentPage('likes');
+  }
+
+  const onClickUser = () => {
+    setCurrentPage('user');
+  }
+
   return (
     <div className={'Footer'}>
       <div className={'Footer__Pages'}>
         <Button
           location={'Footer Home-feed'}
           icon={homeFeedIcon}
+          selected={currentPage === 'home-feed'}
+          onClick={onClickHomeFeed}
         />
         <Button
           location={'Footer Explore'}
           icon={exploreIcon}
+          selected={currentPage === 'explore'}
+          onClick={onClickExplore}
         />
         <Button
           location={'Footer Camera'}
           icon={cameraIcon}
+          selected={currentPage === 'camera'}
+          onClick={onClickCamera}
         />
         <Button
           location={'Footer Likes'}
           icon={likesIcon}
+          selected={currentPage === 'likes'}
+          onClick={onClickLikes}
         />
         <Button
           location={'Footer User'}
           icon={userIcon}
+          selected={currentPage === 'user'}
+          onClick={onClickUser}
         />
       </div>
     </div>
