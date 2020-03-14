25766ada3bb873c94d9631b3445bea46241e8d1a
Han Sanggu <korean139@gmail.com>
2020-03-14 20:52:10 +0900

    feat(page): empty page for home feed

---
+++ src/pages/home-feed/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss';
+import Page from '../../components/page';
+
+const HomeFeed = () => {
+  return (
+    <Page title={'Feed'}>
+      <div className={'Home-feed-container'}>
+      </div>
+    </Page>
+  );
+};
+
+export default HomeFeed;
