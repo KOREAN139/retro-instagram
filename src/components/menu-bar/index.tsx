61936bd64c0da8c12f44f7e266919206517a7390
Han Sanggu <korean139@gmail.com>
2020-03-15 03:30:26 +0900

    fix(component): set menu background when clicked
    not only clicked, but hovered after menu being clicked if mouse never
    left menu bar

--- src/components/menu-bar/index.tsx
+++ src/components/menu-bar/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import './index.scss';
 
 interface MenuBarProps {
@@ -7,21 +8,64 @@ interface MenuBarProps {
 export type Props = MenuBarProps & React.HTMLAttributes<HTMLDivElement>;
 
 const MenuBar: React.FC<Props> = (props) => {
+  const [activated, setActivated] = useState(false);
+
+  const handleOnClick = () => {
+    setActivated(true);
+  };
+
+  const handleMouseLeave = () => {
+    setActivated(false);
+  }
+
   return (
-    <div className={'Menu-bar'}>
-      <div className={'Menu-bar__Menu'}>
+    <div
+      className={'Menu-bar'}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div
+        className={classNames(
+          'Menu-bar__Menu',
+          { 'Activated': activated },
+        )}
+        onClick={handleOnClick}
+      >
         <u>F</u>ile
       </div>
-      <div className={'Menu-bar__Menu'}>
+      <div
+        className={classNames(
+          'Menu-bar__Menu',
+          { 'Activated': activated },
+        )}
+        onClick={handleOnClick}
+      >
         <u>E</u>dit
       </div>
-      <div className={'Menu-bar__Menu'}>
+      <div
+        className={classNames(
+          'Menu-bar__Menu',
+          { 'Activated': activated },
+        )}
+        onClick={handleOnClick}
+      >
         <u>V</u>iew
       </div>
-      <div className={'Menu-bar__Menu'}>
+      <div
+        className={classNames(
+          'Menu-bar__Menu',
+          { 'Activated': activated },
+        )}
+        onClick={handleOnClick}
+      >
         <u>O</u>ptions
       </div>
-      <div className={'Menu-bar__Menu'}>
+      <div
+        className={classNames(
+          'Menu-bar__Menu',
+          { 'Activated': activated },
+        )}
+        onClick={handleOnClick}
+      >
         <u>H</u>elp
       </div>
     </div>
