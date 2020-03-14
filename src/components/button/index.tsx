8889d3e46bb1c247c18456448aeaae45b879a06f
Han Sanggu <korean139@gmail.com>
2020-03-14 21:16:55 +0900

    fix(component): add style for disabled button
    disabled button will not be activated, selected - button's background
    image should be modified by opacity to let user know it's not available

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -6,25 +6,32 @@ interface ButtonProps {
   icon?: string
   location?: string
   selected?: boolean
+  disabled?: boolean
   text?: string
 }
 
 export type Props = ButtonProps & React.HTMLAttributes<HTMLDivElement>;
 
 const Button: React.FC<Props> = (props) => {
-  const { selected, icon, text, ...otherProps } = props;
+  const { selected, icon, text, disabled, ...otherProps } = props;
 
   return (
     <div
       className={classNames(
         'Button',
-        { 'Selected': selected },
+        { 'Able': !disabled },
+        { 'Disabled': disabled },
+        { 'Selected': !disabled && selected },
         props.location,
       )}
       {...otherProps}
     >
       {icon &&
         <div
+          className={classNames(
+            { 'Able': !disabled },
+            { 'Disabled': disabled },
+          )}
           style={{
             backgroundRepeat: 'no-repeat',
             backgroundImage: `url('${icon}')`
