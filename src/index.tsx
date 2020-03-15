41ff9c522bb4fcc3900e0ee9c0eec762a0062c75
Han Sanggu <korean139@gmail.com>
2020-03-15 14:33:42 +0900

    feat(app): prevent refresh on app

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import RootPage from './pages';
 import * as serviceWorker from './serviceWorker';
+import * as Mousetrap from 'mousetrap';
+import 'mousetrap/plugins/global-bind/mousetrap-global-bind';
+
+// prevent refresh on app
+Mousetrap.bindGlobal([
+    'command+r',
+    'command+shift+r',
+    'f5',
+    'ctrl+f5',
+    'ctrl+r'
+  ],
+  () => false
+);
 
 ReactDOM.render(<RootPage />, document.getElementById('root'));
 
