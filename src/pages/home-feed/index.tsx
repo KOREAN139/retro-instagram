6f68b2b7147b9bc478b67ab1517ab9492b381513
Han Sanggu <korean139@gmail.com>
2020-03-25 06:07:03 +0900

    fix(page): render posts with api result

--- src/pages/home-feed/index.tsx
+++ src/pages/home-feed/index.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import './index.scss';
 import Page from '../../components/page';
+import Post from '../../components/post';
+import sample from '../../static/ratedrko.jpg';
+import { RootState } from '../../store';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTimeline } from '../../ducks/instagram';
+import { useHistory } from 'react-router-dom';
 
 const HomeFeed = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const userPk: number = useSelector(
+    (state: RootState) => state.instagram.userPk
+  );
+
+  const timelineInfo = useSelector(
+    (state: RootState) => state.instagram.timelineInfo
+  );
+  const { moreAvailable, posts } = timelineInfo;
+
+  const loadTimelineInfo = useCallback(async () => {
+    if (moreAvailable) {
+      await dispatch(getTimeline(userPk));
+    }
+  }, [dispatch, userPk, moreAvailable]);
+
+  useEffect(() => {
+    loadTimelineInfo();
+  }, [loadTimelineInfo, history]);
+
   return (
     <Page title={'Feed'}>
       <div className={'Home-feed-container'}>
         <div className={'Home-feed-container__Contents'}>
           <div className={'Home-feed-container__Contents__Scrollable'}>
+            {posts && posts.map((post, i) => {
+              return (
+                <Post
+                  source={sample}
+                  postInfo={post.post}
+                  userInfo={post.user}
+                  pixelized={false}
+                  index={i}
+                  key={i}
+                />
+              );
+            })}
           </div>
         </div>
       </div>
