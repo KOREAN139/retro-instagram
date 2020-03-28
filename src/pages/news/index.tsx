3fad69701e2a301c38bea1656d1323eda3ee25de
Han Sanggu <korean139@gmail.com>
2020-03-29 00:17:36 +0900

    fix(page): render news stories in news page
    render actual news stories in news page now

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.scss';
 import Page from '../../components/page';
+import NewsStory from '../../components/news-story';
+import { RootState } from '../../store';
+import { useDispatch, useSelector } from 'react-redux';
+import { getNews } from '../../ducks/instagram';
+import { useHistory } from 'react-router-dom';
 
 const News = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const newsInfo = useSelector(
+    (state: RootState) => state.instagram.newsInfo
+  );
+  const { news } = newsInfo;
+
+  useEffect(() => {
+    const loadNewsInfo = async () => {
+      if (news.length === 0) {
+        await dispatch(getNews());
+      }
+    };
+
+    loadNewsInfo();
+  }, [dispatch, news, history]);
+
   return (
     <Page title={'News'}>
       <div className={'News-page-container'}>
         <div className={'News-page-container__Contents'}>
           <div className={'News-page-container__Contents__Scrollable'}>
+            {news && news.map((newsInfo, i) => {
+              return (
+                <NewsStory
+                  newsInfo={newsInfo}
+                  index={i}
+                  key={i}
+                />
+              );
+            })}
           </div>
         </div>
       </div>
