df5412ec7cd4d3ef700df6e62ff541bd52258a5b
KOREAN139 <korean139@gmail.com>
2020-04-17 17:30:40 +0900

    feat(page): add modal to display user post info
    this box will show like & comment information of user post

---
+++ src/pages/user/post-info-modal/index.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import './index.scss';
+import TitleBar from '@components/title-bar';
+import Button from '@components/button';
+
+interface Props {
+  likeCount: number
+  commentCount: number
+  onClickClose: () => void
+}
+
+const formatText = (likeCount: number, commentCount: number) => {
+  let likePhrase = '';
+  let commentPhrase = '';
+
+  if (likeCount) {
+    likePhrase = `${likeCount} like`;
+    if (likeCount > 1) {
+      likePhrase += 's';
+    }
+  }
+
+  if (commentCount) {
+    commentPhrase = `${commentCount} comment`;
+    if (commentCount > 1) {
+      commentPhrase += 's';
+    }
+  }
+
+  if (likePhrase || commentPhrase) {
+    if (likePhrase && commentPhrase) {
+      return `You earn ${likePhrase} and ${commentPhrase}`;
+    }
+    return `You earn ${likePhrase}${commentPhrase}`;
+  }
+
+  return `You earned no reaction yet :(`;
+};
+
+const PostInfoModal: React.FC<Props> = (props) => {
+  const { onClickClose, likeCount, commentCount } = props;
+
+  return (
+    <>
+      <div className={'Post-info-modal-overlay'} />
+      <div className={'Post-info-modal-container'}>
+        <TitleBar
+          displayIcon={false}
+          location={'Post-info-modal'}
+          title={'Congratulation!'}
+          onClickClose={onClickClose}
+        />
+        <div className={'Post-info-modal-container__Text'}>
+          {formatText(likeCount, commentCount)}
+        </div>
+        <div className={'Post-info-modal-container__Buttons'}>
+          <Button
+            location={'Post-info-modal'}
+            text={'OK'}
+            onClick={onClickClose}
+          />
+          <Button
+            location={'Post-info-modal'}
+            text={'Cancel'}
+            onClick={onClickClose}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PostInfoModal;
