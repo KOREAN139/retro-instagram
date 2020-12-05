93b326b63d565cb66ce630216357beae97f4edc1
Sanggu <korean139@gmail.com>
2020-12-06 02:37:14 +0900

    style(lint): add proper rules for eslint
    those rules are essential for projects maintained w/ typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,34 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb",
-        "prettier",
-        "prettier/@typescript-eslint",
-        "prettier/react"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'simple-import-sort', 'import'],
+  settings: {
+    'import/resolver': {
+      typescript: {},
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        "simple-import-sort",
-        "import"
-    ],
-    "settings": {
-      "import/resolver": {
-        "typescript": {}
-      }
-    },
-    "rules": {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error"
-    }
+  },
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+  },
 };
