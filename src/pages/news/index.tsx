7af76096bdf5c2e23769c647d0409aa7dfa30167
Sanggu <korean139@gmail.com>
2020-12-07 00:03:13 +0900

    style(page): apply lint rules for news page

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,20 +1,19 @@
-import React, { useEffect, useRef } from 'react';
 import './index.scss';
-import Page from '@components/page';
+
 import NewsStory from '@components/news-story';
-import { RootState } from '@store';
-import { useDispatch, useSelector } from 'react-redux';
+import Page from '@components/page';
 import { getNews } from '@ducks/instagram';
+import { RootState } from '@store';
+import React, { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const News = () => {
   const dispatch = useDispatch();
 
-  const newsInfo = useSelector(
-    (state: RootState) => state.instagram.newsInfo
-  );
+  const newsInfo = useSelector((state: RootState) => state.instagram.newsInfo);
   const { news } = newsInfo;
-  const loaded = news.every(news => {
-    const { profilePicture, thumbnail } = news;
+  const loaded = news.every((newsItem) => {
+    const { profilePicture, thumbnail } = newsItem;
     if (!thumbnail) {
       return !!profilePicture.pixelizedMediaUrl;
     }
@@ -45,27 +44,26 @@ const News = () => {
   }, [news, containerRef, bottomRef, dispatch]);
 
   return (
-    <Page
-      title={'News'}
-      loaded={loaded}
-    >
-      <div className={'News-page-container'}>
-        <div className={'News-page-container__Contents'}>
+    <Page title='News' loaded={loaded}>
+      <div className='News-page-container'>
+        <div className='News-page-container__Contents'>
           <div
-            className={'News-page-container__Contents__Scrollable'}
+            className='News-page-container__Contents__Scrollable'
             ref={containerRef}
           >
-            {news && news.map((newsInfo, i) => {
-              return (
-                <NewsStory
-                  newsInfo={newsInfo}
-                  index={i}
-                  key={i}
-                />
-              );
-            })}
+            {news &&
+              news.map((newsItem, i) => {
+                const { profilePicture, createdAt } = newsItem;
+                return (
+                  <NewsStory
+                    newsInfo={newsItem}
+                    index={i}
+                    key={`${profilePicture}_${createdAt}`}
+                  />
+                );
+              })}
             <div
-              className={'News-page-container__Contents__Scrollable__Bottom'}
+              className='News-page-container__Contents__Scrollable__Bottom'
               ref={bottomRef}
             />
           </div>
