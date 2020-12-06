8768968c5781f142a95f0618ba7794a81ce56cd0
Sanggu <korean139@gmail.com>
2020-12-06 23:55:26 +0900

    style(page): apply lint rules for not-used pages

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
 import './index.scss';
+
 import Page from '@components/page';
+import React from 'react';
 
 const Explore = () => {
   return (
-    <Page
-      title={'Explore'}
-      loaded
-    >
-      <div className={'Explore-page-container'}>
-        <div className={'Explore-page-container__Contents'}>
-          <div className={'Explore-page-container__Contents__Scrollable'}>
-          </div>
+    <Page title='Explore' loaded>
+      <div className='Explore-page-container'>
+        <div className='Explore-page-container__Contents'>
+          <div className='Explore-page-container__Contents__Scrollable' />
         </div>
       </div>
     </Page>
