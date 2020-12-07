2d4c4e0445e50bbffce94fd3ba1bf819f98cc3fa
Sanggu <korean139@gmail.com>
2020-12-07 21:21:57 +0900

    sylte(page): apply lint rules for index of page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,18 @@
+import Footer from '@components/footer';
+import MenuBar from '@components/menu-bar';
+import TitleBar from '@components/title-bar';
+import Routes from '@pages/routes';
+import { configureStore, history } from '@store';
+import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
-import { configureStore, history } from '@store';
-import TitleBar from '@components/title-bar';
-import MenuBar from '@components/menu-bar';
-import Routes from '@pages/routes';
-import Footer from '@components/footer';
 
 const store = configureStore();
 
 const RootPage = () => (
   <>
     <Provider store={store}>
-      <TitleBar
-        displayIcon
-        location={'Root'}
-      />
+      <TitleBar displayIcon location='Root' />
       <MenuBar />
       <ConnectedRouter history={history}>
         <Routes />
