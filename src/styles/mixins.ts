591ef48ee80f9595d8bf9b8abf7b8149c45a0306
Sanggu <korean139@gmail.com>
2020-12-25 07:03:14 +0900

    chore(style): remove mixin, placeholder defs

--- src/styles/mixins.ts
+++ src/styles/mixins.ts
@@ -1,19 +1,5 @@
 import { css } from '@emotion/react';
 
-export const font = (fontFamily: string, fontFile: string) => css`
-  @font-face {
-    font-family: ${fontFamily};
-    font-style: normal;
-    font-weight: normal;
-    src: url('../static/fonts/' + ${fontFile} + '.eot');
-    src: url('../static/fonts/' + ${fontFile} + '.eot?#iefix')
-        format('embedded-opentype'),
-      url('../static/fonts/' + ${fontFile} + '.woff') format('woff'),
-      url('../static/fonts/' + ${fontFile} + '.ttf') format('truetype'),
-      url('../static/fonts/' + ${fontFile} + '.svg#aller') format('svg');
-  }
-`;
-
 export const dividerTopShadow = (width = 1) => css`
   box-shadow: inset 0 ${width * 1}px 0 0 rgba(0, 0, 0, 1),
     inset 0 ${width * 2}px 0 0 rgba(255, 255, 255, 1);
